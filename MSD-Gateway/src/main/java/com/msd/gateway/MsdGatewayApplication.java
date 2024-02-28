46054dad6c5ab405d571290bfb722de3363a62f8
Rahulittamalla <101493900+Rahulittamalla@users.noreply.github.com>
2024-02-28 12:38:27 +0530

    Add files via upload

---
+++ MSD-Gateway/src/main/java/com/msd/gateway/MsdGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.msd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsdGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdGatewayApplication.class, args);
+	}
+
+}
---
+++ MSD-Gateway/src/test/java/com/msd/gateway/MsdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msd.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
